ad496e25efdb8f6c90401140262a14c3c8aba0ae
rtuchman <runtuchman@gmail.com>
2018-11-03 16:44:26 +0200

    more tryouts

--- main_sphero.py
+++ main_sphero.py
@@ -159,12 +159,12 @@ class NeuroSpheroManager(object):
 
         return self.create_websocket_connection()
 
-    def disconnect(self, first):
+    def disconnect(self):
         """Close the connection to neuro API and stop the recording."""
         self.neurosphero.buf = {feature: numpy.zeros([self.neurosphero.calibration_samples])
                                 for feature in self.neurosphero.features}
         self.neurosphero.sample_number = 0
-        self.running = False+first
+        self.running = False
         #self.ws.close()
         self.ws_thread = threading.Thread(target=self.idle())
 
