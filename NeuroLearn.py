2d3f42631e0f86e10a1a02aa03edc9c91e119c6e
Ran Tuchman <runtuchman@gmail.com>
2018-12-07 12:49:39 +0200

    nl

--- NeuroLearn.py
+++ NeuroLearn.py
@@ -87,34 +87,31 @@ class NeuroLearnANN(object):
         self.classifier = Sequential()
 
         # Adding the input layer and the first hidden layer
-        # classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu', input_dim = 11))  # Deprecated
-        self.classifier.add(Dense(activation='relu', input_dim=11, units=6, kernel_initializer='uniform'))
+        self.classifier.add(Dense(activation='relu', input_dim=121, units=65, kernel_initializer='uniform'))
 
         # Adding the second hidden layer
-        # classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu'))   # Deprecated
-        self.classifier.add(Dense(units=6, kernel_initializer="uniform", activation='relu'))
+        self.classifier.add(Dense(units=65, kernel_initializer="uniform", activation='relu'))
 
         # Adding the output layer
-        # classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))   # Deprecated
-        self.classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
+        self.classifier.add(Dense(units=3, kernel_initializer='uniform', activation='softmax'))
 
         # Compiling the ANN
-        self.classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+        self.classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
     def data_preprocessing(self):
         # Importing the dataset
-        dataset = pd.read_csv(r'C:\Users\owner\Desktop\NeuroSreer Project\dataset\train_data\neuro_data.csv')
+        dataset = pd.read_csv(r'C:\Users\rtuchman\Desktop\פרויקט גמר\NeuroSphero\dataset\neuro_data.csv')
         X = dataset.iloc[:, 0:121].values
         y = dataset.iloc[:, 121:].values
 
 
         # Splitting the dataset into the Training set and Test set
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
         # Feature Scaling
         sc = StandardScaler()
-        X_train = sc.fit_transform(X_train)
-        X_test = sc.transform(X_test)
+        self.X_train = sc.fit_transform(self.X_train)
+        self.X_test = sc.transform(self.X_test)
 
 
 
@@ -138,7 +135,7 @@ if __name__ == "__main__":
     model = NeuroLearnANN()
     model.data_preprocessing()
     model.train()
-    model.classifier.save_weights('first_try.h5')
+    model.classifier.save('first_try.h5')
 
     print('Done!')
 
